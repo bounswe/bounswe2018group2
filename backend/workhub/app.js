9f0aaf983abaec88189707ad9a5cb46c7f6973a1
Bartu İnce <bartuince97@gmail.com>
2018-10-13 18:47:38 +0300

    Added signup feature without validation

--- backend/workhub/app.js
+++ backend/workhub/app.js
@@ -29,7 +29,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('wah');
 });
 
 db.sequelize
---
+++ backend/workhub/controllers/user_controller.js
@@ -0,0 +1,37 @@
+const db = require('../models');
+const user = db.User;
+
+exports.create = function (req, res) {
+	const { email, firstName, lastName, password } = req.body;
+	//user.validate(req.body);
+	console.log(req.body);
+	console.log('so far so good');
+	user.findOne({
+		where: {email: email}
+	}).then(users => {
+		console.log('searched the database');
+		if (users){
+			res.status(400).send({
+				msg: "This e-mail has already been registered."
+			})
+		} else {
+			console.log('no clashes');
+			user.create({
+				email,
+				firstName,
+				lastName,
+				password,
+			}).then(users => {
+				console.log('Bitchin\'!');
+				res.send({
+					msg: "User successfully created."
+				})
+			}).catch(e => {
+				console.log('oh shit');
+				res.status(500).json({
+					msg: "Something bad happened."
+				})
+			})
+		}
+	})
+} 
