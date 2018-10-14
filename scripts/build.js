1d9fadb3529a2f0876253c69fc666aa8e9d581a1
ergunsh <erdogmusergun@gmail.com>
2018-10-14 19:33:09 +0300

    remove scripts; build frontend and run backend separately

--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-const process = require("child_process");
-const util = require("./util");
-
-util.getBranchPrefix.then(prefix => {
-    if (prefix === util.FRONTEND_PREFIX) {
-        process.exec(`cd frontend && npm run build`);
-    }
-})
