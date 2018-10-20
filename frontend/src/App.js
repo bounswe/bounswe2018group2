2f601872cd2308c25584a96c1e1e306736377d33
ergunsh <erdogmusergun@gmail.com>
2018-10-20 16:52:24 +0300

    use evergreen, add router; update login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
+
+import "./reset.css";
 import "./App.css";
-import Button from '@material-ui/core/Button'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>Hello, React!</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <Button variant="contained" color="primary">
-                        Learn React
-                    </Button>
-                    
-                </a>
-            </header>
-        </div>
+        <Router>
+            <Route exact path="/login" component={LoginPage}/>
+        </Router>
     );
 }
 
