d4789e6b140993b754d37b347d12d28606837cd8
NilsuPC\Nilsu <pamuknilsu@gmail.com>
2018-10-21 20:13:53 +0300

    Revert "login function added to user.controller.js"
    This reverts commit ca57077e760f14b757b75ec6f97297f951fe4547.

--- backend/workhub/controllers/user_controller.js
+++ backend/workhub/controllers/user_controller.js
@@ -45,39 +45,3 @@ exports.create = function (req, res) {
 		}
 	})
 } 
-
-exports.login = function(req,res){
-  var email= req.body.email;
-  var password = req.body.password;
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    // console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    // console.log('The solution is: ', results);
-    if(results.length >0){
-      if(results[0].password == password){
-        res.send({
-          "code":200,
-          "success":"login sucessfull"
-            });
-      }
-      else{
-        res.send({
-          "code":204,
-          "success":"Email and password does not match"
-            });
-      }
-    }
-    else{
-      res.send({
-        "code":204,
-        "success":"Email does not exits"
-          });
-    }
-  }
-  });
-}
