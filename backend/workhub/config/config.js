f0757a10febc57efde28008a12144cece691564f
ergunsh <erdogmusergun@gmail.com>
2018-10-27 17:17:29 +0300

    use dotenv for config things

---
+++ backend/workhub/config/config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const env = process.env.NODE_ENV || "local"
+const resolvedPath = path.resolve(__dirname, "..", `.env.${env}`);
+
+require("dotenv").config({
+  path: resolvedPath
+});
+
+console.log("process.env.MYSQL_HOST", process.env.MYSQL_HOST);
+
+module.exports = {
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql"
+};
