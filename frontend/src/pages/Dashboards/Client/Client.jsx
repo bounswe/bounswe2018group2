abfe49ae2e775822ba8beb2da610e57bcb08896f
ergunsh <erdogmusergun@gmail.com>
2018-10-30 00:08:54 +0300

    add initial Dashboard components

---
+++ frontend/src/pages/Dashboards/Client/Client.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Client extends React.Component {
+    render() {
+        return (
+            <div>client dashboard</div>
+        );
+    }
+}
+
+export default Client;
---
+++ frontend/src/pages/Dashboards/index.js
@@ -0,0 +1,7 @@
+import Client from "./Client";
+import Freelancer from "./Freelancer";
+
+export {
+    Client as ClientDashboard,
+    Freelancer as FreelancerDashboard
+}
