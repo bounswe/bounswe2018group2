74dfc1c9a3d8d662500fa4ecbfca0e1a3569e3e6
ergunsh <erdogmusergun@gmail.com>
2018-10-29 23:36:15 +0300

    add userToken to cors headers

--- backend/workhub/app.js
+++ backend/workhub/app.js
@@ -12,7 +12,7 @@ app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, userToken"
     );
     next();
 });
