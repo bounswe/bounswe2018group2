1ffe52f9df6f598031af16be459820ad9c45d865
ergunsh <erdogmusergun@gmail.com>
2018-10-29 16:47:12 +0300

    fix eslint errors

--- frontend/src/ProfilePages/Client/Client.jsx
+++ frontend/src/ProfilePages/Client/Client.jsx
@@ -1,16 +1,14 @@
-import React, {PropTypes} from 'react';
-import ProfileArea from './client_area.jsx';
+import React from "react";
+import ProfileArea from "./client_area.jsx";
 
 class ClientProfilePage extends React.Component {
-  render() {
-    return (
-      <div className="clientProfilePage">
-        <ProfileArea
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="clientProfilePage">
+                <ProfileArea />
+            </div>
+        );
+    }
 }
-
 
 export default ClientProfilePage;
--- frontend/src/ProfilePages/Freelancer/freelancer.jsx
+++ frontend/src/ProfilePages/Freelancer/freelancer.jsx
@@ -1,16 +1,14 @@
-import React, {PropTypes} from 'react';
-import ProfileArea from './freelancer_area.jsx';
+import React from "react";
+import ProfileArea from "./freelancer_area.jsx";
 
 class FreelancerProfilePage extends React.Component {
-  render() {
-    return (
-      <div className="freelancerProfilePage">
-        <ProfileArea
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="freelancerProfilePage">
+                <ProfileArea />
+            </div>
+        );
+    }
 }
-
 
 export default FreelancerProfilePage;
