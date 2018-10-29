dbd02ccae2056ab5b8cb0a5d0e5b1dd830802ed6
ergunsh <erdogmusergun@gmail.com>
2018-10-29 16:23:38 +0300

    add auth middleware

--- backend/workhub/routes/index.js
+++ backend/workhub/routes/index.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require("express");
+const router = express.Router();
+const user = require("../controllers/user_controller.js");
+const { auth } = require("../middleware");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'hoppa' });
+router.get("/", function(req, res, next) {
+    res.json({ title: "hoppa" });
 });
 
-var user = require('../controllers/user_controller.js');
-router.post('/user/create', user.create);
-router.post('/user/login', user.login);
+router.post("/user/create", user.create);
+router.post("/user/login", user.login);
 
-router.get('/member/:userId', user.profileInfo);
+router.get("/member", auth, user.profileInfo);
+router.get("/member/:userId", auth, user.profileInfo);
 
 module.exports = router;
