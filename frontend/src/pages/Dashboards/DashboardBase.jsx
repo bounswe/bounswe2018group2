1e2b1e6d2d957893c90b9a6ea10b4bded990de65
ergunsh <erdogmusergun@gmail.com>
2018-10-30 12:37:39 +0300

    fix eslint error

--- frontend/src/pages/Dashboards/DashboardBase.jsx
+++ frontend/src/pages/Dashboards/DashboardBase.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import { Pane, Card } from "evergreen-ui";
 import HeaderBar from '../../components/HeaderBar';
 
-class DashboardBase extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <Pane background="#fff" width="100%" height="100%">
-                <HeaderBar/>
-                <Card display="flex" height="100%" background="tint1" margin={5} border="default" elevation={1}>
-                    <Pane flex="1" height="100%" padding={10}></Pane>
-                    <Pane flex="3" height="100%" padding={10}></Pane>
-                </Card>
-            </Pane>
-        );
-    }
+function DashboardBase(props) {
+    return (
+        <Pane background="#fff" width="100%" height="100%">
+            <HeaderBar/>
+            <Card display="flex" height="100%" background="tint1" margin={5} border="default" elevation={1}>
+                <Pane flex="1" height="100%" padding={10}></Pane>
+                <Pane flex="3" height="100%" padding={10}></Pane>
+            </Card>
+        </Pane>
+    );
 }
 
 export default DashboardBase;
