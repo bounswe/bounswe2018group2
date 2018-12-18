a63cfb75c32163ce920588019e4abe8ea509e0d9
ergunsh <erdogmusergun@gmail.com>
2018-12-18 13:34:58 +0300

    Make input components controlled

--- frontend/src/pages/Dashboards/FilterPane/FilterPane.jsx
+++ frontend/src/pages/Dashboards/FilterPane/FilterPane.jsx
@@ -3,7 +3,28 @@ import { Pane, Heading, Combobox, Button, TextInput, Text } from "evergreen-ui";
 import { Paragraph } from "evergreen-ui/commonjs/typography";
 
 class FilterPane extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            category: "",
+            minPrice: "",
+            maxPrice: "",
+            minDuration: "",
+            maxDuration: ""
+        };
+    }
+
     render() {
+        const {
+            category,
+            minPrice,
+            maxPrice,
+            minDuration,
+            maxDuration
+        } = this.props;
+
+        const hasFilterApplied = category || minPrice || maxPrice || !!minDuration || !!maxDuration;
         return (
             <Pane
                 flex="1"
@@ -14,39 +35,42 @@ class FilterPane extends React.Component {
                 border="default"
                 style={{ flexBasis: "25%", position: "sticky", top: 0, height: "min-content" }}>
                 <Heading size={600}>Filter</Heading>
-                <Pane marginTop={30}>
-                    <Heading size={400}>Applied filters</Heading>
-                    <Paragraph>∙ Category: Web Development</Paragraph>
-                    <Paragraph>∙ Price range: 100₺ - 500₺</Paragraph>
-                    <Paragraph>∙ Est. Duration Range: 5 - 10 days</Paragraph>
-                </Pane>
+                {hasFilterApplied && (
+                    <Pane marginTop={30}>
+                        <Heading size={400}>Applied filters</Heading>
+                        <Paragraph>∙ Category: {category}</Paragraph>
+                        <Paragraph>∙ Price range: {minPrice}₺ - {maxPrice}₺</Paragraph>
+                        <Paragraph>∙ Est. Duration Range: {minDuration} - {maxDuration} days</Paragraph>
+                    </Pane>
+                )}
                 <Pane marginTop={30}>
                     <Heading is="h3" size={400}>Category</Heading>
                     <Combobox
                         width="100%"
                         marginTop={5}
                         items={['Banana', 'Orange', 'Apple', 'Mango']}
-                        onChange={selected => console.log(selected)}
+                        selectedItem={this.state.category}
+                        onChange={selected => this.setState({ category: selected })}
                         placeholder="Categories"
                     />
                 </Pane>
                 <Pane marginTop={30}>
                     <Heading is="h3" size={400}>Price</Heading>
                     <Pane display="flex" alignItems="center" marginTop={5}>
-                        <TextInput placeholder="min $" width="auto"/>
+                        <TextInput placeholder="min $" width="auto" value={this.state.minPrice} onChange={e => this.setState({ minPrice: e.target.value })}/>
                         <Text paddingX={5}> - </Text>
-                        <TextInput placeholder="max $" width="auto"/>
+                        <TextInput placeholder="max $" width="auto" value={this.state.maxPrice} onChange={e => this.setState({ maxPrice: e.target.value })}/>
                     </Pane>
                 </Pane>
                 <Pane marginTop={30}>
                     <Heading is="h3" size={400}>Estimated duration</Heading>
                     <Pane display="flex" alignItems="center" marginTop={5}>
-                        <TextInput placeholder="min days" type="number" width="auto"/>
+                        <TextInput placeholder="min days" type="number" width="auto" value={this.state.minDuration} onChange={e => this.setState({ minDuration: e.target.value })}/>
                         <Text paddingX={5}> - </Text>
-                        <TextInput placeholder="max days" type="number" width="auto"/>
+                        <TextInput placeholder="max days" type="number" width="auto" value={this.state.maxDuration} onChange={e => this.setState({ maxDuration: e.target.value })}/>
                     </Pane>
                 </Pane>
-                <Button marginTop={30} width="100%" appearance="primary" intent="success">Apply filter</Button>
+                <Button marginTop={30} width="100%" appearance="primary" intent="success" onClick={() => console.log(this.state)}>Apply filter</Button>
             </Pane>
         );
     }
