1955c7bb1cee830791ab24978f854b8cd1a3cbb0
Bartu İnce <bartuince97@gmail.com>
2018-12-30 17:21:24 +0300

    apidoc updated

--- backend/workhub/apidoc/api_project.js
+++ backend/workhub/apidoc/api_project.js
@@ -1,14 +1,14 @@
 define({
-    name: "workhub",
-    version: "1.0.1",
-    description: "",
-    sampleUrl: false,
-    defaultVersion: "0.0.0",
-    apidoc: "0.3.0",
-    generator: {
-        name: "apidoc",
-        time: "2018-12-22T08:25:18.897Z",
-        url: "http://apidocjs.com",
-        version: "0.17.6"
-    }
+  "name": "workhub",
+  "version": "1.0.1",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-30T14:19:51.946Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
 });
