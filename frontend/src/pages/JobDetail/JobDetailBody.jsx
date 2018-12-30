72dec4de89849dae90f4ac58ea66df8792fc49ee
ergunsh <erdogmusergun@gmail.com>
2018-12-30 19:27:08 +0300

    Handle showing add annotation popup

--- frontend/src/pages/JobDetail/JobDetailBody.jsx
+++ frontend/src/pages/JobDetail/JobDetailBody.jsx
@@ -1,49 +1,74 @@
 import React from "react";
-import { Pane, Paragraph, Heading, Strong, Spinner } from "evergreen-ui";
+import { Pane, Card, Paragraph, Heading, Strong, Spinner, IconButton, Button, Textarea, Label } from "evergreen-ui";
+import debounce from "lodash.debounce";
 
 const options = { year: "numeric", month: "long", day: "numeric" };
 const dateFormatter = new Intl.DateTimeFormat("en-EN", options);
+
+class AnnotationPane extends React.Component {
+    render() {
+        return (
+            <Pane position="absolute" style={{ top: this.props.top, left: this.props.left }}>
+                {this.props.children}
+            </Pane>
+        )
+    }
+}
 
 class JobDetailBody extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            selection: null
+            windowSelection: null,
+            selection: null,
+            annotationRect: null,
+            addingAnnotation: false,
+            annotationText: ""
         };
-        this.handleSelectionChange = this.handleSelectionChange.bind(this);
+
+        this.handleSelectionChange = debounce(this.handleSelectionChange).bind(this);
         this.handleDescriptionRef = this.handleDescriptionRef.bind(this);
+        this.handleWindowClick = this.handleWindowClick.bind(this);
     }
 
     handleSelectionChange() {
-        const selection = window.getSelection();
-        if (selection.isCollapsed) {
+        const windowSelection = window.getSelection();
+        if (windowSelection.isCollapsed) {
             this.setState({
-                selection: null
+                windowSelection: null
             });
             return;
         }
 
-        if (selection.focusNode.nodeType !== Node.TEXT_NODE) {
+        if (windowSelection.focusNode.nodeType !== Node.TEXT_NODE) {
             this.setState({
-                selection: null
+                windowSelection: null
             });
             return;
         }
 
-        if (!this.descriptionEl.contains(selection.focusNode)) {
+        if (!this.descriptionEl.contains(windowSelection.focusNode)) {
             this.setState({
-                selection: null
+                windowSelection: null
             });
             return;
         }
 
-        const range = selection.getRangeAt(0);
+        const range = windowSelection.getRangeAt(0);
         this.setState({
-            selection: {
+            windowSelection: {
                 startOffset: range.startOffset,
-                endOffset: range.endOffset
+                endOffset: range.endOffset,
+                text: windowSelection.toString(),
+                rect: range.getBoundingClientRect()
             }
+        });
+    }
+
+    handleWindowClick() {
+        this.setState({
+            selection: null
         });
     }
 
@@ -52,6 +77,7 @@ class JobDetailBody extends React.Component {
             "selectionchange",
             this.handleSelectionChange
         );
+        window.addEventListener("click", this.handleWindowClick);
     }
 
     componentWillUnmount() {
@@ -59,6 +85,7 @@ class JobDetailBody extends React.Component {
             "selectionchange",
             this.handleSelectionChange
         );
+        window.removeEventListener("click", this.handleWindowClick);
     }
 
     handleDescriptionRef(el) {
@@ -66,7 +93,8 @@ class JobDetailBody extends React.Component {
     }
 
     render() {
-        const { selection } = this.state;
+        const { windowSelection, selection } = this.state;
+
         const { job } = this.props;
         if (!job) {
             return (
@@ -82,6 +110,31 @@ class JobDetailBody extends React.Component {
         return (
             <Pane overflowY="auto">
                 <Heading size={600}>{job.header}</Heading>
+                {selection && (
+                    <>
+                        <Pane position="absolute" top={selection.rect.top} left={selection.rect.left} width={selection.rect.width} height={16} style={{ backgroundColor: "rgba(0, 255, 0, 0.2)"}}>
+                        </Pane>
+                        <AnnotationPane top={selection.rect.bottom} left={selection.rect.right + 10}>
+                            <Card onClickCapture={ev => ev.stopPropagation()} elevation={2} zIndex={100} padding={15} width={320} background="white">
+                                <Label
+                                    htmlFor="textarea-2"
+                                    marginBottom={4}
+                                    display="block">
+                                    Adding annotation
+                                </Label>
+                                <Textarea
+                                    id="textarea-2"
+                                    placeholder={`Add a descriptive annotation for "${selection.text}"`}/>
+                                <Button marginTop={5}>Add annotation</Button>
+                            </Card>
+                        </AnnotationPane>
+                    </>
+                )}
+                {windowSelection && !selection && (
+                    <AnnotationPane left={windowSelection.rect.right}>
+                        <IconButton onClickCapture={(ev) => { ev.stopPropagation();  this.setState(state => { return { addingAnnotation: true, selection: state.windowSelection } })}} icon="annotation"/>
+                    </AnnotationPane>
+                )}
                 <Paragraph marginTop="3px" size={300}>
                     Last updated at{" "}
                     <Strong size={300}>
